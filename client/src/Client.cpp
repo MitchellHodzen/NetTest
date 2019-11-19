3bd74ea7294278f775d6c6b0331300e1c1a9fa88
Mitchell Hodzen <mitchellhodzen@yahoo.com>
2019-11-19 00:09:02 -0500

    converted client to use udpsocket class

--- client/src/Client.cpp
+++ client/src/Client.cpp
@@ -1,75 +1,51 @@
 #include "Client.h"
+#include "UdpSocket.h"
 #include <string>
 void Client::SendMessage()
 {
-		WSADATA wsaData;
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != NO_ERROR)
+	UdpSocket udpSocket(3333);
+	if (udpSocket.Initialize())
 	{
-		std::cout<<"Error starting WSA"<<std::endl;
-		return;
-	}
-
-	int socketHandle = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-
-	if (socketHandle < 0)
-	{
-		std::cout<<"Error creating socket"<<std::endl;
-		return;
-	}
-
-	sockaddr_in address;
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(3333);
-
-	if (bind(socketHandle, (const sockaddr*) &address, sizeof(sockaddr_in)) < 0)
-	{
-		std::cout<<"Error binding socket"<<std::endl;
-		return;
-	}
-
-	//ty gaffer on games https://gafferongames.com/post/sending_and_receiving_packets/
-	unsigned int a = 127;
-	unsigned int b = 0;
-	unsigned int c = 0;
-	unsigned int d = 1;
-
-	unsigned int serverAddr = ( a << 24) | (b << 16) | (c << 8) | d;
-
-	sockaddr_in server;
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = htonl(serverAddr);
-	server.sin_port = htons(2222);
-
-	while(true)
-	{
-		std::string message;
-		std::getline(std::cin, message);
-
-		int sent_size = sendto(socketHandle, message.c_str(), message.size() + 1, 0, (sockaddr*)&server, sizeof(sockaddr_in));
-
-		if (sent_size != message.size() + 1)
+		while(true)
 		{
-			std::cout<<"failed to send packet"<<std::endl;
-			continue;
+
+			if (udpSocket.SetSendAddress(127, 0, 0, 1, 2222))
+			{
+				std::string message;
+				std::getline(std::cin, message);
+				if (udpSocket.Send(message.c_str(), message.size() + 1))
+				{
+					bool receivedResponse = false;
+
+					while(!receivedResponse)
+					{
+						unsigned char dataBuffer[255];
+						unsigned int bufferSize = sizeof(dataBuffer);
+
+						unsigned int fromAddr;
+						unsigned short fromPort;
+
+						if (udpSocket.RecieveMessage((char *)dataBuffer, bufferSize, &fromAddr, &fromPort))
+						{
+							std::cout<<"message recieved from " << fromAddr << ":" << fromPort<< ". Contents: " << dataBuffer <<std::endl;
+							receivedResponse = true;
+						}
+					}
+				}
+				else
+				{
+					std::cout<<"Failed to send packet"<<std::endl;
+				}
+			}
+			else
+			{
+				std::cout<<"Could not set send address"<<std::endl;
+			}
+			
 		}
-
-		//receive messages
-		unsigned char data[255];
-		unsigned int maxPacketSize = sizeof(data);
-
-		sockaddr_in packetFrom;
-		int fromLength = sizeof(packetFrom);
-		int bytes = recvfrom(socketHandle, (char*) data, maxPacketSize, 0, (sockaddr*)&packetFrom, &fromLength);
-
-		unsigned int fromAddress = ntohl(packetFrom.sin_addr.s_addr);
-		unsigned int fromPort = ntohs(packetFrom.sin_port);
-
-		std::cout<<"message recieved from " << fromAddress << ":" << fromPort<< ". Contents: " << data<<std::endl;
 	}
-
-	closesocket(socketHandle);
-
-
-	WSACleanup();
+	else
+	{
+		std::cout<<"Could not initialize socket"<<std::endl;
+	}
 }
