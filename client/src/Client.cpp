9a2d3c1b71cb464c6f4275eabfecd0dc108bbb9f
Mitchell Hodzen <mitchellhodzen@yahoo.com>
2020-07-12 16:28:11 -0400

    added IP and port input for client

--- client/src/Client.cpp
+++ client/src/Client.cpp
@@ -8,48 +8,80 @@ void Client::SendMessage()
 	UdpSocket udpSocket(3333);
 	if (udpSocket.Initialize())
 	{
-		//while(true)
-		//{
+		std::cout<<"Input IP and Port in aaaa.bbbb.cccc.dddd:xxxx format: ";
+		std::string ipInput;
+		std::cin >> ipInput;
 
-			if (udpSocket.SetSendAddress(127, 0, 0, 1, 2222))
+		size_t last = 0;
+		size_t next = 0;
+		unsigned char partA;
+		unsigned char partB;
+		unsigned char partC;
+		unsigned char partD;
+		unsigned short port;
+
+		//IPs have exactly 4 parts, delimited by a '.'
+		size_t currentPartIndex = 0;
+		size_t lastPartIndex = 0;
+
+		currentPartIndex = ipInput.find('.', lastPartIndex);
+		partA = std::stoi(ipInput.substr(lastPartIndex, currentPartIndex-lastPartIndex));
+
+		lastPartIndex = currentPartIndex + 1;
+		currentPartIndex = ipInput.find('.', lastPartIndex);
+		partB = std::stoi(ipInput.substr(lastPartIndex, currentPartIndex-lastPartIndex));
+
+		lastPartIndex = currentPartIndex + 1;
+		currentPartIndex = ipInput.find('.', lastPartIndex);
+		partC = std::stoi(ipInput.substr(lastPartIndex, currentPartIndex-lastPartIndex));
+
+		//Last part is ended by a ':', followed by the port
+		lastPartIndex = currentPartIndex + 1;
+		currentPartIndex = ipInput.find(':', lastPartIndex);
+		partD = std::stoi(ipInput.substr(lastPartIndex, currentPartIndex-lastPartIndex));
+
+		lastPartIndex = currentPartIndex + 1;
+		port = std::stoi(ipInput.substr(lastPartIndex));
+
+		//if (udpSocket.SetSendAddress(127, 0, 0, 1, 2222))
+		if (udpSocket.SetSendAddress(partA, partB, partC, partD, port))
+		{
+			TestStruct test;
+			test.networkId = 500;
+			test.integer = -15;
+			test.floatingPoint = 14.8495f;
+			int size = sizeof(TestStruct);
+			
+			if (udpSocket.Send(&test, size))
 			{
-				TestStruct test;
-				test.networkId = 500;
-				test.integer = -15;
-				test.floatingPoint = 14.8495f;
-				int size = sizeof(TestStruct);
+				bool receivedResponse = false;
+
+				unsigned char dataBuffer[255];
+				unsigned int bufferSize = sizeof(dataBuffer);
+
+				unsigned int fromAddr;
+				unsigned short fromPort;
 				
-				if (udpSocket.Send(&test, size))
+				while(!receivedResponse)
 				{
-					bool receivedResponse = false;
 
-					unsigned char dataBuffer[255];
-					unsigned int bufferSize = sizeof(dataBuffer);
-
-					unsigned int fromAddr;
-					unsigned short fromPort;
-					
-					while(!receivedResponse)
+					if (udpSocket.RecieveMessage(dataBuffer, bufferSize, &fromAddr, &fromPort))
 					{
-
-						if (udpSocket.RecieveMessage(dataBuffer, bufferSize, &fromAddr, &fromPort))
-						{
-							std::cout<<"message recieved from " << fromAddr << ":" << fromPort<< ". Contents: " << dataBuffer <<std::endl;
-							receivedResponse = true;
-						}
+						std::cout<<"message recieved from " << fromAddr << ":" << fromPort<< ". Contents: " << dataBuffer <<std::endl;
+						receivedResponse = true;
 					}
-				}
-				else
-				{
-					std::cout<<"Failed to send packet"<<std::endl;
 				}
 			}
 			else
 			{
-				std::cout<<"Could not set send address"<<std::endl;
+				std::cout<<"Failed to send packet"<<std::endl;
 			}
-			
-		//}
+		}
+		else
+		{
+			std::cout<<"Could not set send address"<<std::endl;
+		}
+		
 	}
 	else
 	{
