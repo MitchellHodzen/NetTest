5b3731c10f8f8030cc967f26df581f52f7ae3a88
Mitchell Hodzen <mitchellhodzen@yahoo.com>
2020-07-14 22:56:45 -0400

    adding simple session connection

--- server/include/Server.h
+++ server/include/Server.h
@@ -1,8 +1,20 @@
 #pragma once
 #include <iostream>
+#include <stack>
+#include <unordered_map>
+
 class Server
 {
 public:
+	Server();
 	void StartServer();
+private:
+	const unsigned int MAX_CONNECTIONS = 2;
+	std::stack<unsigned int> availableNetworkIds;
+	std::unordered_map<unsigned int, unsigned int> addressNetworkIdMap;
+
+	bool AttemptNewConnection(unsigned int address, unsigned int* networkId);
+	void DisconnectSession(unsigned int address);
+
 };
 
--- server/src/Server.cpp
+++ server/src/Server.cpp
@@ -2,6 +2,13 @@
 #include "UdpSocket.h"
 #include "MessageStructs.h"
 
+Server::Server()
+{
+	for (int i = 0; i < MAX_CONNECTIONS; ++i)
+	{
+		availableNetworkIds.push(i);
+	}
+}
 void Server::StartServer()
 {
 	UdpSocket udpSocket(2222);
@@ -41,4 +48,47 @@ void Server::StartServer()
 			}
 		}
 	}
+}
+
+
+bool Server::AttemptNewConnection(unsigned int address, unsigned int* networkId)
+{
+	//If the address is already connected, use that network ID
+	if (addressNetworkIdMap.find(address) != addressNetworkIdMap.end())
+	{
+		//If not, map the address to the network ID
+		*networkId = addressNetworkIdMap.at(address);
+		return true;
+	}
+
+	//If there are no available network IDs, reject connection
+	if (availableNetworkIds.empty())
+	{
+		std::cout<<"Connection from address " << address << " refused, no more network IDs available"<<std::endl;
+		return false;
+	}
+
+	//If there are available network IDs, get the next available network ID and map it to the address
+	unsigned int nextNetworkId = availableNetworkIds.top();
+	availableNetworkIds.pop();
+	addressNetworkIdMap.insert_or_assign(address, nextNetworkId);
+	*networkId = nextNetworkId;
+	return true;
+}
+void Server::DisconnectSession(unsigned int address)
+{
+
+	//Only disconnect if the address isn't connected
+	if (addressNetworkIdMap.find(address) != addressNetworkIdMap.end())
+	{
+		//Get the network ID associated with that address
+		unsigned int networkId = addressNetworkIdMap.at(address);
+		//Remove value from address network ID map
+		addressNetworkIdMap.erase(address);
+		//Add the network ID back to the available network IDs
+		availableNetworkIds.push(networkId);
+
+		//Send a disconnect packet
+	}
+
 }
